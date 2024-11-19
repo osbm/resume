96a46dfa3270e108f2f0bb2a50c1595367d8299e
osbm <osmanfbayram@gmail.com>
2024-11-19 23:02:28 +0300

    nice flake package

--- flake.nix
+++ flake.nix
@@ -9,13 +9,17 @@
     };
   };
 
-  outputs = { self , nixpkgs ,... }: let
+  outputs = { self , nixpkgs , typst-packages, ... }: let
     system = "x86_64-linux";
     pkgs = import nixpkgs {
         inherit system;
     };
     environment_packages = with pkgs; [
       typst
+      font-awesome
+      font-awesome_5
+      source-sans
+      source-sans-pro
     ];
   in {
     devShells."${system}".default = pkgs.mkShell {
@@ -24,25 +28,29 @@
         echo 'Welcome to the development environment of my resume.'
       '';
     };
-    packages."${system}".default = pkgs.stdenvNoCC.mkDerivation {
-        name = "build-pdf";
-        src = ./.;
-        nativeBuildInputs = environment_packages;
-        buildPhase = ''
-          typst compile resume.typ
-          echo $XDG_CACHE_HOME
-          mkdir -p $out
-          cp resume.pdf $out/
+    packages."${system}" = {
+      default = pkgs.stdenvNoCC.mkDerivation {
+          name = "build-pdf";
+          src = ./.;
+          nativeBuildInputs = environment_packages ++ [ self.packages."${system}".typst-packages-package ];
+          # buildInputs = [ pkgs.typst-packages ];
+          # use the path of the cache to store the resume
+          # XDG_CACHE_HOME =
+          buildPhase = ''
+            export XDG_CACHE_HOME=${self.packages."${system}".typst-packages-package}
+            mkdir -p $out
+            typst compile resume.typ $out/resume.pdf
+          '';
+      };
+      typst-packages-package = pkgs.stdenv.mkDerivation {
+        name = "typst-packages-cache";
+        src = "${typst-packages}/packages";
+        dontBuild = true;
+        installPhase = ''
+          mkdir -p "$out/typst/packages"
+          cp -LR --reflink=auto --no-preserve=mode -t "$out/typst/packages" "$src"/*
         '';
-    };
-    packages."${system}".typst-packages = stdenv.mkDerivation {
-      name = "typst-packages-cache";
-      src = typstPackagesSrc;
-      dontBuild = true;
-      installPhase = ''
-        mkdir -p "$out/typst/packages"
-        cp -LR --reflink=auto --no-preserve=mode -t "$out/typst/packages" "$src"/*
-      '';
+      };
     };
   };
 }
