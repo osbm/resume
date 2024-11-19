0df6a284fc5fc06abcc7d2ae2060dc2dde65703f
osbm <osmanfbayram@gmail.com>
2024-11-19 12:03:40 +0300

    add nix flakes for development

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "For now it only includes typst to build my resume.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    # nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self , nixpkgs ,... }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+        inherit system;
+    };
+    environment_packages = with pkgs; [
+      typst
+    ];
+  in {
+    devShells."${system}".default = pkgs.mkShell {
+      packages = environment_packages;
+      shellHook = ''
+        echo 'Welcome to the development environment of my resume.'
+      '';
+    };
+    packages."${system}".default = pkgs.stdenvNoCC.mkDerivation {
+        name = "build-pdf";
+        src = ./.;
+        nativeBuildInputs = environment_packages;
+        buildPhase = ''
+	  typst compile resume.typ
+	  mkdir -p $out
+	  cp resume.pdf $out/
+	'';
+        installPhase = ''
+          echo install phase started
+        '';
+    };
+  };
+}
