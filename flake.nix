1bc0fca4022fbc4daa52ac80afcb3868e2817beb
osbm <osmanfbayram@gmail.com>
2024-11-19 23:05:39 +0300

    try out typix

--- flake.nix
+++ flake.nix
@@ -1,56 +1,120 @@
 {
-  description = "For now it only includes typst to build my resume.";
+  description = "My resume project";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    typix = {
+      url = "github:loqusion/typix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+
     typst-packages = {
       url = "github:typst/packages";
       flake = false;
     };
+
+    # Example of downloading icons from a non-flake source
+    # font-awesome = {
+    #   url = "github:FortAwesome/Font-Awesome";
+    #   flake = false;
+    # };
   };
 
-  outputs = { self , nixpkgs , typst-packages, ... }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-        inherit system;
-    };
-    environment_packages = with pkgs; [
-      typst
-      font-awesome
-      font-awesome_5
-      source-sans
-      source-sans-pro
-    ];
-  in {
-    devShells."${system}".default = pkgs.mkShell {
-      packages = environment_packages;
-      shellHook = ''
-        echo 'Welcome to the development environment of my resume.'
-      '';
-    };
-    packages."${system}" = {
-      default = pkgs.stdenvNoCC.mkDerivation {
-          name = "build-pdf";
-          src = ./.;
-          nativeBuildInputs = environment_packages ++ [ self.packages."${system}".typst-packages-package ];
-          # buildInputs = [ pkgs.typst-packages ];
-          # use the path of the cache to store the resume
-          # XDG_CACHE_HOME =
-          buildPhase = ''
-            export XDG_CACHE_HOME=${self.packages."${system}".typst-packages-package}
-            mkdir -p $out
-            typst compile resume.typ $out/resume.pdf
-          '';
+  outputs = inputs @ {
+    nixpkgs,
+    typix,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      inherit (pkgs) lib;
+
+      typixLib = typix.lib.${system};
+
+      src = typixLib.cleanTypstSource ./.;
+      commonArgs = {
+        typstSource = "main.typ";
+
+        fontPaths = [
+          # Add paths to fonts here
+          # "${pkgs.roboto}/share/fonts/truetype"
+        ];
+
+        virtualPaths = [
+          # Add paths that must be locally accessible to typst here
+          # {
+          #   dest = "icons";
+          #   src = "${inputs.font-awesome}/svgs/regular";
+          # }
+        ];
       };
-      typst-packages-package = pkgs.stdenv.mkDerivation {
+      typstPackagesSrc = pkgs.symlinkJoin {
+        name = "typst-packages-src";
+        paths = [
+          "${inputs.typst-packages}/packages"
+          # More Typst packages can be added here
+        ];
+      };
+      typstPackagesCache = pkgs.stdenv.mkDerivation {
         name = "typst-packages-cache";
-        src = "${typst-packages}/packages";
+        src = typstPackagesSrc;
         dontBuild = true;
         installPhase = ''
           mkdir -p "$out/typst/packages"
           cp -LR --reflink=auto --no-preserve=mode -t "$out/typst/packages" "$src"/*
         '';
       };
-    };
-  };
+
+      # Compile a Typst project, *without* copying the result
+      # to the current directory
+      build-drv = typixLib.buildTypstProject (commonArgs
+        // {
+          inherit src;
+          XDG_CACHE_HOME = typstPackagesCache;
+        });
+
+      # Compile a Typst project, and then copy the result
+      # to the current directory
+      build-script = typixLib.buildTypstProjectLocal (commonArgs
+        // {
+          inherit src;
+          XDG_CACHE_HOME = typstPackagesCache;
+        });
+
+      # Watch a project and recompile on changes
+      watch-script = typixLib.watchTypstProject commonArgs;
+    in {
+      checks = {
+        inherit build-drv build-script watch-script;
+      };
+
+      packages.default = build-drv;
+
+      apps = rec {
+        default = watch;
+        build = flake-utils.lib.mkApp {
+          drv = build-script;
+        };
+        watch = flake-utils.lib.mkApp {
+          drv = watch-script;
+        };
+      };
+
+      devShells.default = typixLib.devShell {
+        inherit (commonArgs) fontPaths virtualPaths;
+        packages = [
+          # WARNING: Don't run `typst-build` directly, instead use `nix run .#build`
+          # See https://github.com/loqusion/typix/issues/2
+          # build-script
+          watch-script
+          # More packages can be added here, like typstfmt
+          # pkgs.typstfmt
+        ];
+      };
+    });
 }
+
