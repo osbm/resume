ba8ba8859ddb8adf24d23ef114e6d39eb13a7dff
osbm <osmanfbayram@gmail.com>
2024-11-19 14:32:09 +0300

    add add typst packages package

--- flake.nix
+++ flake.nix
@@ -29,13 +29,20 @@
         src = ./.;
         nativeBuildInputs = environment_packages;
         buildPhase = ''
-	  typst compile resume.typ
-	  mkdir -p $out
-	  cp resume.pdf $out/
-	'';
-        installPhase = ''
-          echo install phase started
+          typst compile resume.typ
+          echo $XDG_CACHE_HOME
+          mkdir -p $out
+          cp resume.pdf $out/
         '';
+    };
+    packages."${system}".typst-packages = stdenv.mkDerivation {
+      name = "typst-packages-cache";
+      src = typstPackagesSrc;
+      dontBuild = true;
+      installPhase = ''
+        mkdir -p "$out/typst/packages"
+        cp -LR --reflink=auto --no-preserve=mode -t "$out/typst/packages" "$src"/*
+      '';
     };
   };
 }
