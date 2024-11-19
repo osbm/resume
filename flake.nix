47b38629f90de8e9e871b53ad15f45263fffd9dc
osbm <osmanfbayram@gmail.com>
2024-11-19 14:06:31 +0300

    Revert "try out new flake"
    This reverts commit b3074d0b3c96ac071735c8a161a06c22d9d3f79c.

--- flake.nix
+++ flake.nix
@@ -1,102 +1,38 @@
 {
-  description = "A Typst project";
+  description = "For now it only includes typst to build my resume.";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-    typix = {
-      url = "github:loqusion/typix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    flake-utils.url = "github:numtide/flake-utils";
-
-    # Example of downloading icons from a non-flake source
-    # font-awesome = {
-    #   url = "github:FortAwesome/Font-Awesome";
-    #   flake = false;
-    # };
-
-    inputs.typst-packages = {
-      url = "github:typst/packages";
-      flake = false;
-    };
-
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    # nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = inputs @ {
-    nixpkgs,
-    typix,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-      inherit (pkgs) lib;
-
-      typixLib = typix.lib.${system};
-
-      src = typixLib.cleanTypstSource ./.;
-      commonArgs = {
-        typstSource = "resume.typ";
-
-        fontPaths = [
-          # Add paths to fonts here
-          # "${pkgs.roboto}/share/fonts/truetype"
-        ];
-
-        virtualPaths = [
-          # Add paths that must be locally accessible to typst here
-          # {
-          #   dest = "icons";
-          #   src = "${inputs.font-awesome}/svgs/regular";
-          # }
-        ];
-      };
-
-      # Compile a Typst project, *without* copying the result
-      # to the current directory
-      build-drv = typixLib.buildTypstProject (commonArgs
-        // {
-          inherit src;
-        });
-
-      # Compile a Typst project, and then copy the result
-      # to the current directory
-      build-script = typixLib.buildTypstProjectLocal (commonArgs
-        // {
-          inherit src;
-        });
-
-      # Watch a project and recompile on changes
-      watch-script = typixLib.watchTypstProject commonArgs;
-    in {
-      checks = {
-        inherit build-drv build-script watch-script;
-      };
-
-      packages.default = build-drv;
-
-      apps = rec {
-        default = watch;
-        build = flake-utils.lib.mkApp {
-          drv = build-script;
-        };
-        watch = flake-utils.lib.mkApp {
-          drv = watch-script;
-        };
-      };
-
-      devShells.default = typixLib.devShell {
-        inherit (commonArgs) fontPaths virtualPaths;
-        packages = [
-          # WARNING: Don't run `typst-build` directly, instead use `nix run .#build`
-          # See https://github.com/loqusion/typix/issues/2
-          # build-script
-          watch-script
-          # More packages can be added here, like typstfmt
-          # pkgs.typstfmt
-        ];
-      };
-    });
+  outputs = { self , nixpkgs ,... }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+        inherit system;
+    };
+    environment_packages = with pkgs; [
+      typst
+    ];
+  in {
+    devShells."${system}".default = pkgs.mkShell {
+      packages = environment_packages;
+      shellHook = ''
+        echo 'Welcome to the development environment of my resume.'
+      '';
+    };
+    packages."${system}".default = pkgs.stdenvNoCC.mkDerivation {
+        name = "build-pdf";
+        src = ./.;
+        nativeBuildInputs = environment_packages;
+        buildPhase = ''
+	  typst compile resume.typ
+	  mkdir -p $out
+	  cp resume.pdf $out/
+	'';
+        installPhase = ''
+          echo install phase started
+        '';
+    };
+  };
 }
